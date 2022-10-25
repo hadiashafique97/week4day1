220531bdf6ec86c55cdf030a5e66bd8a9f4564c9
Hadia Shafique <hs@Hadias-MBP.attlocal.net>
2022-10-25 12:35:20 -0500

    added function 2

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ console.log('first commit')
 
 
 //FUNCTION 1 commit changes to git hub 
-const logNumbers = () => {
-    for(i = 1 ; i<= 20; i++){
-       console.log(i) 
+// const logNumbers = () => {
+//     for(i = 1 ; i<= 20; i++){
+//        console.log(i) 
+//     }
+// }
+// logNumbers()
+
+//FUNCTION 2 commit to git hub write a forloop that will log only the even numbers 0 to 200
+
+const logEven = () => {
+    for (i = 0; i <= 200; i++){
+
+        if(i % 2 == 0 && i !== 0){
+            console.log(i)
+        }
     }
 }
-logNumbers()
-
-//FUNCTION 2 commit to git hub
-
+logEven()
 
 
 //FUNCTION 3 commit to github
