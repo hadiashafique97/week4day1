c0f10b61a785ea9555cd3aa7b94687dcfbbf8de1
Hadia Shafique <hs@Hadias-MBP.attlocal.net>
2022-10-25 12:14:29 -0500

    made fucntion1

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 console.log('first commit')
+
+
+//FUNCTION 1 commit changes to git hub 
+const logNumbers = () => {
+    for(i = 1 ; i<= 20; i++){
+       console.log(i) 
+    }
+}
+logNumbers()
+
+//FUNCTION 2 commit to git hub
+
+
+
+//FUNCTION 3 commit to github
+
+
+
+//FUNCTION 4 commit to github
+
+
+
