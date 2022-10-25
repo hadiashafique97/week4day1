dad9bbf656a1e228f7a3139788bf1699b387670a
Hadia Shafique <hs@Hadias-MBP.attlocal.net>
2022-10-25 12:50:04 -0500

    added wild wild life

--- index.js
+++ index.js
@@ -40,5 +40,22 @@ getMyNumbers()
 
 //FUNCTION 4 commit to github
 
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
 
+plantee [2] += 1
+console.log(plantee)
 
+wolfy[3]= "gotham City"
+console.log(wolfy)
+
+dart[3] += ' Hawkings'
+console.log(dart)
+
+// wolfy[0] = "GameBoy"
+wolfy.shift()
+wolfy.unshift('Gameboy')
+console.log(wolfy)
